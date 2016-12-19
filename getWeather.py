a2336d0fa413be27956e6a24fc4a3ed6b16c49e7
Dimitrios Belis <dimitrios.belis@williamhill.com>
2016-12-19 18:13:19 +0000

    Working script for weather updates

---
+++ getWeather.py
@@ -0,0 +1,41 @@
+#! /usr/bin/python3
+
+import pyowm
+
+owm = pyowm.OWM('b3362437408e57646f5c22fe4e576f8e')
+
+try:
+    observation = owm.weather_at_place("Sheffield,GB")
+    current_weather = observation.get_weather()
+
+    detailed = current_weather.get_detailed_status()
+    short = current_weather.get_status()
+    humidity = current_weather.get_humidity()
+    temp = current_weather.get_temperature('celsius')
+    max_temp = temp['temp_max']
+    min_temp = temp['temp_min']
+    cur_temp = temp['temp']
+
+    #print(detailed)
+    #print(short)
+    #print(humidity)
+    #print(round(max_temp))
+    #print(min_temp)
+    #print(round(cur_temp,1))
+
+    if cur_temp > 22:
+        temp_col = 'red'
+    else:
+        if cur_temp > 13:
+            temp_col = 'green'
+        else:
+            temp_col = 'cyan'
+
+    file = open("/home/vagrant/openweathermap.out", "w")
+    file.write("#[bg=%s] #[fg=white][%s] %s#[fg=black] | %s/%s #[fg=default]#[bg=default]" % (temp_col,short,round(cur_temp,1),round(min_temp),round(max_temp)))
+    file.close()
+except:
+    file = open("/home/vagrant/openweathermap.out", "w")
+    file.write("#[bg=%s] #[fg=white][%s] #[fg=default]#[bg=default]" % ('black', 'N/A'))
+    file.close()
+ 
