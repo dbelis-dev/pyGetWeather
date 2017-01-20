b4c08dc0ce00a89cbf3e17eb1974325fb7d21952
Dimitrios Belis <dimitrios.belis@williamhill.com>
2017-01-20 12:24:25 +0000

    Reworked getTomorrow, getCurrent scripts

--- getCurrentWeather.py
+++ getCurrentWeather.py
@@ -4,25 +4,19 @@ import pyowm
 #import stringcase
 
 raised_exc = 0
+x = 0
 
 owm = pyowm.OWM('b3362437408e57646f5c22fe4e576f8e')
 
 try:
     observation = owm.weather_at_place("Sheffield,uk")
     current_weather = observation.get_weather()
-    today_forecasts = owm.daily_forecast("Sheffield,uk", limit=1)
-    today_forecast = today_forecasts.get_forecast()
 except:
     raised_exc = -1
 else:
-    #short = current_weather.get_status()
     detailed = current_weather.get_detailed_status()
     temp = current_weather.get_temperature('celsius')
     cur_temp = temp['temp']
-    for weather in today_forecast:
-        temp_fc = weather.get_temperature('celsius')
-        max_temp = temp_fc['max']
-        min_temp = temp_fc['min']
 
     if cur_temp > 22:
         temp_col = 'red'
--- getTomorrowsWeather.py
+++ getTomorrowsWeather.py
@@ -1,29 +1,35 @@
 #! /usr/bin/python3
 
 import pyowm
+#import stringcase
 
 raised_exc = 0
+x = 0
 
 owm = pyowm.OWM('b3362437408e57646f5c22fe4e576f8e')
 
 try:
-    forecast = owm.three_hours_forecast("Sheffield,uk")
-    tomorrow_morning = pyowm.timeutils.tomorrow(6,0)
-    tomorrow_morning_forecast = forecast.get_weather_at(tomorrow_morning).get_status()
-    tomorrow_noon = pyowm.timeutils.tomorrow(12,0)
-    tomorrow_noon_forecast = forecast.get_weather_at(tomorrow_noon).get_status()
-    tomorrow_evening = pyowm.timeutils.tomorrow(18,0)
-    tomorrow_evening_forecast = forecast.get_weather_at(tomorrow_evening).get_status()
-    tomorrow_night = pyowm.timeutils.tomorrow(23,59)
-    tomorrow_night_forecast = forecast.get_weather_at(tomorrow_night).get_status()
+    multiday_forecasts = owm.daily_forecast("Sheffield,uk", limit=2)
+    multiday_forecast = multiday_forecasts.get_forecast()
 except:
     raised_exc = -1
 else:
+    for weather in multiday_forecast:
+        ## skip today's forecast
+        if x > 0:
+            temp_fc = weather.get_temperature('celsius')
+            max_temp = temp_fc['max']
+            min_temp = temp_fc['min']
+            detail_fc = weather.get_detailed_status()
+        else:
+            x += 1
+            pass
+
     if raised_exc == -1:
         str_out_3 = "#[bg=%s] #[fg=white][%s] #[fg=default]#[bg=default]" % ('black', 'N/A')
     else:
-        str_out_3_pre = "#[bg=green]#[fg=black]" 
-        str_out_3 = "%s | %s | %s | %s" % (tomorrow_morning_forecast, tomorrow_noon_forecast, tomorrow_evening_forecast, tomorrow_night_forecast)
+        str_out_3_pre = "#[bg=yellow]#[fg=black]"
+        str_out_3 = "%s [%s / %s]" % (detail_fc.title(), round(min_temp,1), round(max_temp,1))
         str_out_3_post = "#[fg=default]#[bg=default]"
 
     file = open("/Users/dbelis/openweathermap.3", "w")
